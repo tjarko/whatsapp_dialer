f059834dc576462892c509a246b2f743793892d5
Tjarko Leifer <tjarko@gmail.com>
2025-05-09 09:47:13 -0700

    add github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/whatsapp_dialer' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig 
